1a5c9ae7a5e34599130cf98d0a882847db11a4ac
Emam hossain <hossainemam12345@gmail.com>
2024-04-11 21:45:11 +0600

    besic vue routing

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterView, RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">about</RouterLink>
+      <RouterLink to="/contact">contact</RouterLink>
+    </nav>
+    <div>
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+nav a {
+  display: inline-block;
+  margin-left: 2rem;
+  padding: 1rem;
+}
+
+nav a.exact-active-class {
+  background-clip: green;
+}
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+   <h1>contact page</h1>
+  </main>
+</template>
